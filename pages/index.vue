53ba69407ea6101dd88a616879ba39f35f196d69
Alireza Ahmadi <77675646+AwlirezaZ@users.noreply.github.com>
2023-09-22 19:59:31 +0330

    add some

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
      <div class="flex h-full">
         <div class="bg-gray-300 w-1/3 ">
            <div class="flex justify-center h-full items-center">
-             <div class="justify-center" @click="timerCountUp()">
+             <div class="justify-center" @click="timerCountDown()">
               <svg  height="200px" width="200px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000">
                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -14,8 +14,7 @@
                     </g>
                  </g>
               </svg>
-              <div >TIMER:</div>
-              <div >{{ minute}}:{{ second % 60 }}</div>
+              <div class="mx-auto " ><span class="text-8xl font-bold w-full">{{ minute }}:<span v-if="second <10">0</span>{{ second }}</span></div>
            </div>
 
               
@@ -24,42 +23,65 @@
         <div class="bg-white w-2/3">
           <div class="flex justify-end justify-items-end w-full  p-8">
             <div class="px-4 h-1/18 justify-center flex">
-              <svg class="w-10 mx-7" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>profile_image_round [#1326]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2199.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M342,2055.615 C342,2055.722 341.97,2055.821 341.939,2055.918 C341.723,2052.974 339.918,2050.482 337.375,2049.283 C338.368,2048.369 339,2047.071 339,2045.615 C339,2043.534 337.728,2041.753 335.92,2041 L341,2041 C341.552,2041 342,2041.063 342,2041.615 L342,2055.615 Z M339.963,2057 L327.975,2057 C327.974,2057 327.969,2056.741 327.969,2056.701 C327.969,2053.605 330.326,2050.96 333.339,2050.645 C334,2050.733 334.255,2050.622 334.623,2050.576 C337.625,2050.902 339.969,2053.623 339.969,2056.71 C339.969,2056.75 339.964,2057 339.963,2057 L339.963,2057 Z M326,2055.615 L326,2041.615 C326,2041.063 326.448,2041 327,2041 L332.08,2041 C330.272,2041.753 329,2043.534 329,2045.615 C329,2047.06 329.622,2048.351 330.602,2049.264 C328.107,2050.422 326.307,2052.82 326.012,2055.675 C326.011,2055.654 326,2055.636 326,2055.615 L326,2055.615 Z M337,2045.615 C337,2047.055 335.979,2048.26 334.623,2048.548 C334.033,2048.5 333.868,2048.508 333.368,2048.545 C332.017,2048.254 331,2047.052 331,2045.615 C331,2043.961 332.346,2042.615 334,2042.615 C335.654,2042.615 337,2043.961 337,2045.615 L337,2045.615 Z M342,2039 L326,2039 C324.895,2039 324,2039.895 324,2041 L324,2057 C324,2058.104 324.895,2059 326,2059 L342,2059 C343.105,2059 344,2058.104 344,2057 L344,2041 C344,2039.895 343.105,2039 342,2039 L342,2039 Z" id="profile_image_round-[#1326]"> </path> </g> </g> </g> </g></svg>
-              <span class="mx-7 text-lg">Profile</span>
               <svg class="w-10 mx-7" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="analysis"> <polyline class="cls-1" points="3.96 14.04 9 9 15 15 23 7"></polyline> <line class="cls-1" x1="2.54" y1="15.46" x2="1" y2="17"></line> </g> </g></svg>
               <span class="mx-7 text-lg">Statics</span>
+              <svg class="w-10 mx-7" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>profile_image_round [#1326]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2199.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M342,2055.615 C342,2055.722 341.97,2055.821 341.939,2055.918 C341.723,2052.974 339.918,2050.482 337.375,2049.283 C338.368,2048.369 339,2047.071 339,2045.615 C339,2043.534 337.728,2041.753 335.92,2041 L341,2041 C341.552,2041 342,2041.063 342,2041.615 L342,2055.615 Z M339.963,2057 L327.975,2057 C327.974,2057 327.969,2056.741 327.969,2056.701 C327.969,2053.605 330.326,2050.96 333.339,2050.645 C334,2050.733 334.255,2050.622 334.623,2050.576 C337.625,2050.902 339.969,2053.623 339.969,2056.71 C339.969,2056.75 339.964,2057 339.963,2057 L339.963,2057 Z M326,2055.615 L326,2041.615 C326,2041.063 326.448,2041 327,2041 L332.08,2041 C330.272,2041.753 329,2043.534 329,2045.615 C329,2047.06 329.622,2048.351 330.602,2049.264 C328.107,2050.422 326.307,2052.82 326.012,2055.675 C326.011,2055.654 326,2055.636 326,2055.615 L326,2055.615 Z M337,2045.615 C337,2047.055 335.979,2048.26 334.623,2048.548 C334.033,2048.5 333.868,2048.508 333.368,2048.545 C332.017,2048.254 331,2047.052 331,2045.615 C331,2043.961 332.346,2042.615 334,2042.615 C335.654,2042.615 337,2043.961 337,2045.615 L337,2045.615 Z M342,2039 L326,2039 C324.895,2039 324,2039.895 324,2041 L324,2057 C324,2058.104 324.895,2059 326,2059 L342,2059 C343.105,2059 344,2058.104 344,2057 L344,2041 C344,2039.895 343.105,2039 342,2039 L342,2039 Z" id="profile_image_round-[#1326]"> </path> </g> </g> </g> </g></svg>
+              <span class="mx-7 text-lg">Profile</span>
             </div>
           </div>
-          <div class="w-full py-200 h-1/3 my-12 ">
+          <div class="w-full py-200 h-1/3 my-12" v-if="timerIsStarted">
             
             <div class="w-1/2 py-1 mx-auto">
-              <h1 class="text-lg">NEW TASK</h1>
-              <input class="block w-full my-10 h-12 border-2 border-black" type="text" placeholder="name" />
-              <input class="block w-full my-10 h-12 border-2 border-black" type="text" placeholder="email"/>
-              <button class="block w-full my-10 h-12 border-2 border-black bg-black rounded-lg"><span class="text-white">START</span></button>
-              <button class="block w-full my-10 h-12 border-2 bg-gray-300 rounded-lg" ><span class="text-color">CANCEL</span></button>
-
-
+              <h1 class="text-lg font-bold">NEW TASK</h1>
+              <input class="block w-full mt-10 h-12 border-2 border-black px-3 font-bold text-black" v-model="name" type="text" placeholder="Name" />
+              <span v-if="nameIsValid === false" class="text-red-500">THIS FIELD IS REQUIRED!</span>
+              <input class="block w-full mt-10 h-12 border-2 border-black px-3 font-bold" v-model="email" type="text" placeholder="Email"/>
+              <span v-if="mailIsValid === false" class="text-red-500">EMAIL FORMAT IS INVALID!</span>
+              <button class="block w-full mt-8 h-12 border-2 border-black bg-black rounded-2xl" @click="submit()"><span class="text-white">START</span></button>
+              <button class="block w-full mt-4 h-12 border-2 bg-gray-300 rounded-2xl" ><span class="text-color">CANCEL</span></button>
             </div>
           </div>
         </div>
      </div>
   </div>
 </template>
-<script>
-  const timer = ref(5)
+<script setup>
+
+  const name = ref('')
+  const timerIsStarted = ref(false)
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  const email = ref('')
   const second = ref(0)
-  const minute = ref(0)
-onMounted(() => {
-  timerCountUp();
-  });
-  const timerCountUp = () =>{
+  const minute = ref(25)
+  const mailIsValid =ref()
+  const nameIsValid = ref()
+  const timerCountDown = () =>{
+    if(!timerIsStarted.value){
     setInterval(() =>{
-        second.value++
-        if(second % 60 === 0 ){
-          minute.value++
+      timerIsStarted.value =true;
+        if(second.value != 0){
+          second.value--
+        }
+        else{
+          minute.value--
+          second.value = 59
         }
     },1000)
+
+  }
+  }
+  const submit = () =>{
+    validateData();
+  }
+  const validateData = () =>{
+    mailIsValid.value = validateEmail(email.value)
+    nameIsValid.value = validateName(name.value)
+  }
+  const validateEmail = (email) =>{
+    return emailRegex.test(email);
+  }
+  const validateName = (name) =>{
+    return name.trim() !== "";
   }
 </script>
 <style scoped></style>
